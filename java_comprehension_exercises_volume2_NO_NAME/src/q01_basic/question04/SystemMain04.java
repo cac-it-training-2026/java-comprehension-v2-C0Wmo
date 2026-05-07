580a327b7d420f5eb07f376dc179f15cd0670cc8
takuriha <takuriha@C010423243.zipangu.cac.co.jp>
2026-05-07 14:04:26 +0900

    FIX q04_basic.question04まで完了 理解できた点 ・staticメソッドの作成
    不明な点
    ・引数が配列の場合のメソッド？

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question04/SystemMain04.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question04/SystemMain04.java
@@ -5,7 +5,6 @@ public class SystemMain04 {
 	public static void main(String[] args) {
 
 		Member[] members = new Member[2];
-		//TODO ここから実装する
 
 		Member miura = new Member(1, "Passw0rd", "Miura Manabu", 28, 2);
 		members[0] = miura;
